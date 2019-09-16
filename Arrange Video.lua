05d18f634606a804c46451652b4fd883b4985225
Conny <cornelia.lin@tum.de>
2019-09-16 16:24:22 +0200

    Implement entry removal

--- Arrange Video.lua
+++ Arrange Video.lua
@@ -18,32 +18,76 @@ for i=1, #aspr.tags do
 end
 
 -- Sequences
-local seq_num = 0
 local sequences = {}
 
--- Add sequence by number
-function AddSequence()
-	-- Save entry changes between last + click and now
-	local j = 1
-	while d.data["tag_"..j] do
-		sequences[j] = d.data["tag_"..j]
-		j = j+1
+-- Save entry changes between clicks and create new dialog
+function commitChanges()
+	local i = 1
+	while d.data["tag_"..i] do
+		sequences[i] = d.data["tag_"..i]
+		i = i+1
 	end
+end
 
+-- Close current dialog and create a new copy that can still be changed
+function recreateEntries()
 	d:close()
 	d = Dialog("Arrange Sequences")
-	
-	-- Recreate old entries
 	for i=1, #sequences do
 		d:combobox{id="tag_"..i, label="Tag "..i..": ", option=sequences[i], options=tags}
+		 :button{id="rm_"..i, text="-", onclick=function() rmSequence(i) end}
 	end
-	-- Add new entry
+end
+
+-- Add sequence to the end of the list
+function addSequence()
+	commitChanges()
+	recreateEntries()
 	d:combobox{id="tag_"..#sequences+1, label="Tag "..#sequences+1 ..": ", options=tags}
-	d:button{id="add", text="+", onclick=function() AddSequence() end}
+	 :button{id="rm_"..#sequences+1, text="-", onclick=function() rmSequence(#sequences+1) end}
+	 :button{id="add", text="+", onclick=function() addSequence() end}
+	 :show()
+end
+
+-- Remove sequence from anywhere in the list
+function rmSequence(nr)
+	-- Commit previous additions if applicable
+	commitChanges()
+	-- Delete entries and adjust list
+	table.remove(sequences, nr)
+	while d.data["tag_"..nr] do
+		d.data["tag_"..nr] = d.data["tag_"..nr+1]
+		nr = nr+1
+	end
+	-- Recreate panel without that dialog
+	recreateEntries()
+	d:button{id="add", text="+", onclick=function() addSequence() end}
 	 :show()
 end
 
 -- Dialog widgets
 d:label{id="help", label="", text="Arrange your tags"}
- :button{id="add", text="+", onclick=function() AddSequence() end}
+ :button{id="add", text="+", onclick=function() addSequence() end}
  :show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
