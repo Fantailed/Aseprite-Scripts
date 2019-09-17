ca038460a219c6bfe6ac7399fca0c2ca54f357f1
Conny <cornelia.lin@tum.de>
2019-09-17 15:32:38 +0200

    Implement copy for reverse and ping-pong

--- Generate.lua
+++ Generate.lua
@@ -43,7 +43,7 @@ end
 
 function copyBackward(from, tag_length)
 	for l=1, #aspr.layers do
-		for f=tag_length, 1 do
+		for f=tag_length, 1, -1 do
 			-- Add entry for cel if exists
 			if cel_map[l.."-"..(from + f)] then
 				table.insert(cel_seq, {idx=cel_map[l.."-"..(from + f)], layer=l, frame=frame_dst + (tag_length - f)})
@@ -63,8 +63,9 @@ function copyTagContent(tag_idx)
 	elseif tag.aniDir == 1 then							-- Reverse
 		copyBackward(from, tag_length)
 	elseif tag.aniDir == 2 then 						-- Ping-Pong
-		copyForward(from, tag_length-1)
+		copyForward(from, tag_length)
 		copyBackward(from, tag_length)
+		frame_dst = frame_dst - 1
 	end
 end
 
@@ -84,12 +85,13 @@ function export()
     -- Create the new sprite
 	local new_spr = Sprite(aspr.spec)
 	
-	-- Calculate cel arrangement for new sprite
+	-- Calculate required copy operations and cel arrangement for new sprite
 	generateSequence()
 	
-	--[[ Debug Output
+	---[[ Debug Output
+	print("Cel sequence: ")
 	for i=1, #cel_seq do
-		print("Cel seq info: "..cel_seq[i].idx..", "..cel_seq[i].layer..", "..cel_seq[i].frame)
+		print("Source cel index: "..cel_seq[i].idx.."; Destination layer: "..cel_seq[i].layer.."; Destination frame: "..cel_seq[i].frame)
 	end
 	--]]
 	
@@ -101,17 +103,17 @@ function export()
 		new_spr:newEmptyFrame()
 	end
 	
-	-- Iterate through each cel
+	-- Iterate through calculated copy sequence
 	for i, cel in ipairs(cel_seq) do
 		-- Copy cel content
 		local image = aspr.cels[cel.idx].image
 		local new_cel = new_spr:newCel(new_spr.layers[cel.layer], cel.frame, image, aspr.cels[1].position)
 	end
-	--]]
+	d:close()
 end
 
 
-local d = Dialog("Arrange Sequences")
+local d = Dialog("Arrange Clip")
 
 d:button{id="gen", text="Export", onclick=function() export() end}
  :button{text="cancel"}
